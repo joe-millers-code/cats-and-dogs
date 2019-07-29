0bccf3edfd1131b641caf81ec2180776b3b4e376
github username <github email address>
2019-07-29 17:57:46 -0400

    comment crud and basic staticpages#home set up

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,53 @@
 class CommentsController < ApplicationController
+    before before_action :find_comment, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @comment = Comment.all
+    end
+
+    def show
+    end
+
+    def new
+        @comment = Comment.new
+    end
+
+    def edit
+    end
+
+    def create
+        @comment = Member.new(comment_params)
+        if @comment.valid?
+            @comment.save
+            redirect_to comment_path(@comment)
+        else
+            flash[:errors] = @comment.errors.full_messages
+            redirect_to new_comment_path
+        end
+    end
+
+    def update
+        if @comment.update(comment_params)
+            redirect_to comment_path(@comment)
+        else 
+            flash[:errors] = @comment.errors.full_messages
+            redirect_to edit_comment_path(@comment)
+        end
+    end
+
+    def destroy
+        @comment.destroy
+        redirect_to comments_path
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:dog, :cat, :content)
+    end
+
+    def find_comment
+        @comment = Comment.find(params[:id])
+    end
 end
