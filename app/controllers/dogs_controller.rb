dd331f3f2115fb40f9a5ea6fdd017f7c49f9831f
github username <github email address>
2019-07-29 17:55:45 -0400

    added full crud for dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,58 @@
 class DogsController < ApplicationController
+    before_action :find_dog, only: [:show, :edit, :update, :destroy]
+
+  
+        def index
+            @dogs = Dog.all
+        end
+    
+        def show
+        end
+    
+        def new 
+            @dog = Dog.new 
+        end
+    
+        def create
+            @dog = Dog.new(dog_params)
+    
+            if  @dog.valid? 
+                @dog.save
+                redirect_to @dog
+            else 
+                flash[:errors] = @dog.errors.full_messages
+                redirect_to new_employee_path    
+            end
+    
+        end
+    
+        def edit 
+            
+        end
+    
+        def update 
+            if @dog.update(dog_params)
+                redirect_to @dog
+            else 
+                flash[:errors] = @dog.errors.full_messages
+                redirect_to edit_dog_path
+            end
+        end
+
+        def destroy
+
+            @dog.destroy
+            redirect_to dogs_path
+          end
+    
+    
+        private 
+    
+        def find_dog
+            @dog = Dog.find(params[:id])
+        end
+    
+        def dog_params
+            params.require(:dog).permit(:name, :age, :city, :breed, :bio, :temperament, :picture)
+        end
 end
