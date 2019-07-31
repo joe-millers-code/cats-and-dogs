cccb3405eec2133d1fbfd3e5542d59119c8297c6
github username <github email address>
2019-07-31 14:19:44 -0400

    comment functionality basic

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -6,27 +6,27 @@ class LoginController < ApplicationController
     end
 
     def create 
-        if params[:cat]
-        @cat = Cat.find_by(username: params[:username])
-        if @cat && @cat.authenticate(params[:password])
-        flash["message"] = "Meow! Successfully logged in, #{@cat.name}! Check out these dogs!"
-        session[:cat_id] = @cat.id 
-        redirect_to dogs_path
-        else 
-        flash["message"] = "Hiss! Incorrect username or password!"
-        redirect_to login_path
+        if 
+            @cat = Cat.find_by(username: params[:username])
+            if @cat && @cat.authenticate(params[:password])
+                flash["message"] = "Meow! Successfully logged in, #{@cat.name}! Check out these dogs!"
+                session[:cat_id] = @cat.id 
+                redirect_to dogs_path
+            else 
+                flash["message"] = "Hiss! Incorrect username or password!"
+                redirect_to login_path
+            end
+        elsif 
+            @dog = Dog.find_by(username: params[:username])
+            if @dog && @dog.authenticate(params[:password])
+                flash["message"] = "Woof! Successfully logged in, #{@dog.name}! Check out these cats!"
+                session[:dog_id] = @dog.id 
+                redirect_to cats_path
+            else 
+                flash["message"] = "Growl! Incorrect username or password!"
+                redirect_to login_path
+            end
         end
-    elsif params[:dog]
-        @dog = Dog.find_by(username: params[:username])
-        if @dog && @dog.authenticate(params[:password])
-        flash["message"] = "Woof! Successfully logged in, #{@dog.name}! Check out these cats!"
-        session[:dog_id] = @dog.id 
-        redirect_to cats_path
-        else 
-        flash["message"] = "Growl! Incorrect username or password!"
-        redirect_to login_path
-        end
-    end
     end
 
     def destroy 
